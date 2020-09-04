101f3262fd11962df780a2827db700d60a1b4510
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-09-04 18:19:31 +0300

    Support multiple dist configurations

--- action/commands/build.d.ts
+++ action/commands/build.d.ts
@@ -1,6 +1,5 @@
 export declare const buildCommand: import("../command").CommandFactory<{}, {
     single?: boolean | undefined;
 }>;
-export declare function readPackageJson(baseDir: string): Promise<any>;
 export declare function validatePackageJson(pkg: any): void;
 //# sourceMappingURL=build.d.ts.map
